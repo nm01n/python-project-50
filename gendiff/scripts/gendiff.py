8d14072782fcc37d52508caa6d0ef5cfb7cec02b
Olga <nemo0mb@gmail.com>
2025-11-02 17:02:48 +0300

    Add gendiff CLI script with argparse

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Generate diff between two files."""
+
+import argparse
+
+
+def main():
+    """Run gendiff CLI."""
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    
+    args = parser.parse_args()
+    print(f"Comparing {args.first_file} and {args.second_file}")
+
+
+if __name__ == '__main__':
+    main()
